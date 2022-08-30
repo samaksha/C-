744770b0d57d203d655759b9aa70b5937a13f73e
samaksha <kumarsamaksha21@gmail.com>
2022-08-30 11:30:11 +0530

    Calculator final

--- KnowledgeBase/Js/script2.js
+++ KnowledgeBase/Js/script2.js
@@ -1,23 +1,47 @@
 
 var res = '';
-function equalTo(event)
+function equalTo()
 {
     try {
-        var ans = eval();
+        var ans = eval(res);
+        res = ans;
+        if(ans == undefined)
+            throw "undefined";
     }
     catch(err)
     {
-        display.innerHTML = err.message;
-        res = '';
+        console.log(err);
+        res = "";
     }
-    event.stopPropagation();
+    display.innerHTML = res;
 }
 
 function addOperand(event,value)
 {
-    console.log(value);
+    console.log(event["path"][1]);
     console.log(event);
-    var text  = value.innerText;
+    var text  = event["path"][1].innerText;
+    res = res + text;
+    display.innerHTML = res;
+    event.stopPropagation();
+}
+
+function addOperator(event)
+{
+    console.log(event);
+    var text = event["path"][0].innerText;
+    console.log(text);
+    if(text == "=")
+    {
+        equalTo();
+        event.stopPropagation();
+        return;
+    }
+    if(text == "AC")
+    {
+        res = '';
+        text = '';
+    }
     res = res + text;
     display.innerHTML = res;
     event.stopPropagation();
@@ -36,4 +60,11 @@ for([key,value] of operandbtnarr){
     value.addEventListener("click",function(e){
         addOperand(e,value);
     });
+}
+
+var operator = document.getElementsByClassName("operator");
+for(var i = 0;i<operator["length"];i++)
+{
+    var x = i.toString();
+    operator[x].addEventListener("click",addOperator);
 }
